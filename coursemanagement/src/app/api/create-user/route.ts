9920fa2c9aa993865e3747197a1330b88520faec
n2ai <longhai2511@gmail.com>
2024-05-11 23:10:10 -0500

    Work on add new user

--- coursemanagement/src/app/api/create-user/route.ts
+++ coursemanagement/src/app/api/create-user/route.ts
@@ -1,9 +1,21 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
+import bcrypt, { hash } from "bcryptjs";
 import { json } from 'stream/consumers';
  
 export async function POST(request: Request) {
-  console.log(JSON.parse(request.content))
+  
+  const {firstName, lastName, email, password} = await request.json();
 
-  return NextResponse.json('Got the call')
+  const hashedPassword = await bcrypt.hash(password,10);
+
+  
+  try{
+    const result = await sql`INSERT INTO users (firstName, lastName, email, password)
+                            VALUES(${firstName},${lastName},${email},${hashedPassword});`;
+
+    return NextResponse.json({message:'Successfully created account'},{status:200});
+  }catch(error){
+    return NextResponse.json({error},{status:500});
+  }
 }
